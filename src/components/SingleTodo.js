3e220699a70217968a8d4a043afe2bb1ce4a8b2b
Kevin Truong <kevintru0ng@yahoo.com>
2018-06-04 16:08:31 -0700

    init

---
+++ src/components/SingleTodo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+const SingleTodo = props => {
+  return (
+    <li>
+      {props.todo}<button onClick={props.delete}>X</button>
+    </li>
+  )
+}
+
+export default SingleTodo
